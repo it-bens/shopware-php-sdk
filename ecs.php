8d3834a794c858bca03148abfc2b61825f0b25ac
Martin Bens <martin.bens@it-bens.de>
2024-10-13 18:01:13 +0200

    Directories added to Rector ECS configuration.

--- ecs.php
+++ ecs.php
@@ -30,6 +30,8 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+
     $ecsConfig->import(SetList::CLEAN_CODE);
     $ecsConfig->import(SetList::COMMON);
     $ecsConfig->import(SetList::PSR_12);
